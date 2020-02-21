b3a89819e3c5fc941790267141a6d3919ff34624
Aman Agrawal <amanagrawal2098@gmail.com>
2020-02-21 20:42:35 +0530

    signup data is being registered

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,82 @@
 var express = require('express');
 var router = express.Router();
-
+var userModule=require('../modules/user');
+var bcrypt=require('bcryptjs');
 /* GET home page. */
+
+function checkEmail(req,res,next){
+  var email=req.body.email;
+  var checkexitemail=userModule.findOne({email:email});
+  checkexitemail.exec((err,data)=>{
+ if(err) throw err;
+ if(data){
+  
+return res.render('signup', { title: 'Password Management System', msg:'Email Already Exit' });
+//this is returned so that if the email check fails it will restart the page other it will not go to this page.
+ }
+ next();
+  });
+}
+function checkUsername(req,res,next){
+  var uname=req.body.uname;
+  var checkexituser=userModule.findOne({username:uname});
+  checkexituser.exec((err,data)=>{
+ if(err) throw err;
+ if(data){
+  
+return res.render('signup', { title: 'Password Management System', msg:'Username Already Exit' });
+
+ }
+ next();
+  });
+}
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Password Management System' });
+  res.render('index', { title: 'Password Management System',msg:"" });
 });
 
 router.get('/signup', function(req, res, next) {
-  res.render('signup', { title: 'Password Management System' });
+  res.render('signup', { title: 'Password Management System',msg:"" });
+});
+
+router.post('/signup',checkUsername,checkEmail, function(req, res, next) {
+  var username=req.body.uname;
+  var password=req.body.password;
+  var confpassword=req.body.confpassword;
+  var email=req.body.email;
+ 
+  if(password!=confpassword)
+  {
+    res.render('signup', { title: 'Password Management System',msg:"Confirm Password doesn't match" });
+  }
+  else{
+    
+    password=bcrypt.hashSync(req.body.password,10);
+    var userDetails=new userModule({
+      username:username,
+      email:email,
+      password:password,
+        });
+    userDetails.save((err,data)=>{
+    if(err) throw err;
+    res.render('signup', { title: 'Password Management System',msg:"User succesfully registered" });
+      });
+    }
+  
 });
 
 router.get('/passwordCategory', function(req, res, next) {
-  res.render('password_category', { title: 'Password Management System' });
+  res.render('password_category', { title: 'Password Management System',msg:"" });
 });
 
 router.get('/add-new-category', function(req, res, next) {
-  res.render('addNewCategory', { title: 'Password Management System' });
+  res.render('addNewCategory', { title: 'Password Management System' ,msg:""});
 });
 
 router.get('/add-new-password', function(req, res, next) {
-  res.render('add-new-password', { title: 'Password Management System' });
+  res.render('add-new-password', { title: 'Password Management System',msg:"" });
 });
 router.get('/view-all-password', function(req, res, next) {
-  res.render('view-all-password', { title: 'Password Management System' });
+  res.render('view-all-password', { title: 'Password Management System',msg:"" });
 });
 module.exports = router;
